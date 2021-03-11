4ac70f117242ce10c8e6cd4c342bd7ebf5f9e791
Joao-Otavio-SB <joogui2010@hotmail.com>
2021-03-11 16:49:49 -0300

    added

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,51 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
     title: "Contador de Pessoas",
-    home: Stack(
+    home: contPessoas()
+  ));
+}
+
+class contPessoas extends StatefulWidget{
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<contPessoas>{
+
+  int _people = 0;
+  String _capacity = "Pode Entar!";
+  Color textColor = Colors.white;
+
+  void _numberPeople(int n){
+    setState(() {
+      _people += n;
+
+      if(_people == 10){
+      _capacity = "Lotado!";
+      textColor = Colors.red;
+     }else{
+      _capacity = "Pode Entrar!";
+      textColor = Colors.green;
+      }
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Stack(
       children: [
         Image.asset(
           "img/restaurant_bg.jpg",
           fit: BoxFit.cover,
+          height: 1000,
         ),
         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              "Pessoas: 3",
+              "Pessoas: $_people",
               style: TextStyle(color: Colors.white, fontWeight: FontWeight.normal),
             ),
             Row(
@@ -22,9 +56,13 @@ void main(){
                 Padding(
                   padding: EdgeInsets.all(20),
                   child: TextButton(
-                    onPressed: (){},
+                    onPressed: (){
+                      if(_people > 0){
+                        _numberPeople(-1);
+                      }
+                    },
                     child: Text(
-                      "+1",
+                      "-1",
                       style: TextStyle(color: Colors.white, fontSize: 40)
                     )
                   ),
@@ -32,9 +70,13 @@ void main(){
                 Padding(
                   padding: EdgeInsets.all(20),
                   child: TextButton(
-                    onPressed: (){},
+                    onPressed: (){
+                      if(_people < 10){
+                        _numberPeople(1);
+                      }
+                    },
                     child: Text(
-                      "-1",
+                      "+1",
                       style: TextStyle(color: Colors.white, fontSize: 40)
                     )
                   ),
@@ -42,12 +84,12 @@ void main(){
                 ],
               ),
               Text(
-                "Pode Entrar!",
-                style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+                "$_capacity",
+                style: TextStyle(color: textColor, fontSize: 30, fontWeight: FontWeight.bold),
               )
           ],
         )
       ],
-    )
-  ));
+    );
+  }
 }
